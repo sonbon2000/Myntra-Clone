fcd8a59cb98263e489dcc8a951fd11818a794417
Tran Sy Hoang Son <sonbon1101@gmail.com>
2022-05-30 13:26:40 +0700

    implement blog, blog detail, contact

---
+++ src/app/components/blog/blog-detail/blog-detail.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Doc, RootObject } from 'src/app/shared/models/blog.model';
+import { ProductService } from 'src/app/shared/services/product.service';
+
+@Component({
+  selector: 'app-blog-detail',
+  templateUrl: './blog-detail.component.html',
+  styleUrls: ['./blog-detail.component.scss'],
+})
+export class BlogDetailComponent implements OnInit {
+  id: string = '';
+  blog: Doc;
+  image;
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.id = params['id'];
+      this.productService
+        .getSingleBlog(this.id)
+        .subscribe((res: RootObject) => {
+          this.blog = res.response.docs[0];
+          let multimedia = res.response.docs[0].multimedia;
+          if (multimedia.length == 0) {
+            this.image =
+              'https://assets.teenvogue.com/photos/5b3cead4cd6b096ecd587a3a/4:3/w_824,h_618,c_limit/how-to-be-a-fashion-blogger-keiko-lynn-tout.jpg';
+          } else {
+            for (let i = 0; i < multimedia.length; i++) {
+              this.image = 'https://www.nytimes.com/' + multimedia[i].url;
+              break;
+            }
+          }
+        });
+    });
+  }
+
+  onNavigate() {
+    this.router.navigate(['/blog']);
+  }
+}
--- src/app/shared/services/product.service.ts
+++ src/app/shared/services/product.service.ts
@@ -1,15 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Doc, RootObject } from '../models/blog.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  private getAllURL = 'https://fakestoreapi.com/products';
+  private API_KEY = 'xDgMAhzQV1pf0klr5wIjkDanmC1H6r3u';
+  private getProducts = 'https://fakestoreapi.com/products';
+  private getBlogs = `https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=news_desk:(%22Fashion%22)&api-key=${this.API_KEY}`;
   constructor(private http: HttpClient) {}
 
   getAllProducts(): Observable<any> {
-    return this.http.get<any>(this.getAllURL);
+    return this.http.get<any>(this.getProducts);
+  }
+
+  getAllBlogs(): Observable<RootObject> {
+    return this.http.get<RootObject>(this.getBlogs);
+  }
+
+  getSingleBlog(id: string): Observable<RootObject> {
+    return this.http.get<RootObject>(
+      `https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=xDgMAhzQV1pf0klr5wIjkDanmC1H6r3u&fq=_id:(%22${id}%22)`
+    );
   }
 }
